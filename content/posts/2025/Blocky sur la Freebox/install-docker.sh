5602fa88f74ce5a5d42698ed2632a68a5ebb1b85
Julien Noblet <julien.noblet@gmail.com>
2025-04-02 22:48:32 +0200

    Ajout de nouveaux fichiers d'images, de listes de blocage et de configuration pour le projet Blocky sur la Freebox

---
+++ content/posts/2025/Blocky sur la Freebox/install-docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/{{ .chezmoi.osRelease.id }}/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker $USER
+
+# EOF
