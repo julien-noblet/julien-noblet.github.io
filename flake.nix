3e323b8592b93a7fca7526acd403b2c90424e317
Julien Noblet <julien.noblet@gmail.com>
2026-05-27 23:16:34 +0200

    chore: add Nix flake development environment configuration

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Julien Noblet's blog development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            hugo
+            nodejs
+            git
+            gnumake
+          ];
+
+          shellHook = ''
+            echo "--------------------------------------------------"
+            echo " Welcome to the blog development environment!     "
+            echo " Hugo: $(hugo version | cut -d' ' -f2)"
+            echo " Node: $(node --version)"
+            echo " Git:  $(git --version | cut -d' ' -f3)"
+            echo " Make: $(make --version | head -n1)"
+            echo "--------------------------------------------------"
+          '';
+        };
+      });
+    };
+}
